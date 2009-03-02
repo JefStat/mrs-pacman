4527d7f083adecb1a9d4f1097477dd1d721b1fe6
nwaldrum@connect.carleton.ca <nwaldrum@connect.carleton.ca@08c9650a-f779-11dd-b507-8b4ecd5210be>
2009-03-02 01:12:40 +0000

    Fixed the character and ghost classes as per Michael's recommendation

---
+++ MrsPacMan/src/Ghost.java
@@ -0,0 +1,33 @@
+import java.awt.Point;
+/* 
+ * 
+ */
+
+
+public class Ghost extends Character {
+	private final int CHASER = 0;
+	private final int AMBUSHER = 1;
+	private final int FICKLE = 2;
+	private final int STUPID = 3;
+	
+	private boolean incarcerated;
+	private boolean scared;
+	private boolean scatter;
+	private int personality;
+	
+	public void Ghost(int p){
+		Ghost 
+	}
+	
+	public void movetoPacMan(PacMan P){
+		
+	}
+	
+	public void movetoPrison(Point p){
+		
+	}
+	
+	public void runAway(PacMan p){
+		
+	}
+}
