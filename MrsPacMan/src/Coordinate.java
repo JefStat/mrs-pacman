6f0b14fcc3899f5cdd0a176c7cca1abc34c59c9e
nnasser <nnasser@08c9650a-f779-11dd-b507-8b4ecd5210be>
2009-03-02 04:53:24 +0000

    Coordinate Class Completed

--- MrsPacMan/src/Coordinate.java
+++ MrsPacMan/src/Coordinate.java
@@ -8,31 +8,38 @@ import java.awt.Point;
  * Title: Coorindate Class version 1.1
  * Date: February 16, 2009
  * Author: Nicole Waldrum and Jef Statham
+ * Milestone 2: WRITTEN/MODIFIED by Nahim Nasser
+ * Updates: Functions added and modified
  */
 
-public class Coordinate {
+@SuppressWarnings("serial")
+public class Coordinate extends Point{
 	
-	private final int EMPTY = 0;
-	private final int WALL = 1;
-	private final int PACDOT = 2;
-	private final int POWERPELLET = 3;
-	private final int CHEERIES = 4;
+	public final static int EMPTY = 0;
+	public final static int WALL = 1;
+	public final static int PACDOT = 2;
+	public final static int POWERPELLET = 3;
+	public final static int CHEERIES = 4;
+	public final static int PRISON = 5;
 	
-	private Point position; 
 	private int identity;
 	
 	/*
 	 * Default constructor sets an identiy to a map point p
 	 */
-	public Coordinate(Point p){
-		
+	public Coordinate(int x, int y, int identity){
+		super(x,y);
+		this.identity = identity;
 	}
-	
 	/*
 	 * returns the identiy of point p on the map
 	 */
-	public int GetIdentity(Point p){
-		return -1;
+	public int getIdentity(){
+		return identity;
+	}
+	
+	public void setIdentity(int x){
+		this.identity = x;
 	}
 	
 }
